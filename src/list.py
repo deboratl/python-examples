0c539aebded2dd8ff0274dc2df349f7754551b86
deboratleal <deboora.alves2001@gmail.com>
2023-07-07 21:11:15 -0300

    first commit; Trabalhando com listas

---
+++ src/list.py
@@ -0,0 +1,10 @@
+animals = ["cachorro", "gato", "pato", "passaro", 999]
+print(animals)
+animals.append("tartaruga")
+print(animals)
+animals.remove(999)
+print(animals)
+animals.insert(3, "macaco")
+print(animals)
+_len = len(animals)
+print(_len)
